73a917410936184e33f8f8a8c22872a1ff25d92e
truepatch <truepatch@gmail.com>
2022-10-12 10:43:01 +0500

    void

--- void.ts
+++ void.ts
@@ -3,3 +3,26 @@ export {};
 function logId(id: string | number): void {
     console.log(id);
 }
+const a = logId(1);
+
+function multiplay(f: number, s?: number) {
+    if (!s) {
+        return f * f;
+    }
+    // return f * s;
+}
+
+type voidFunc = () => void;
+
+const f1: voidFunc = () => {};
+const f2: voidFunc = () => {
+    return true;
+};
+
+const b = f2(); //return void
+
+const skills = ['Dev', 'DevOps']
+const user = {
+	s: ['s']
+}
+skills.forEach((skill) => user.s.push(skill))
